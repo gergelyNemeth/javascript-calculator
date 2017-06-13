2fbe04e3798b24acb6eeb65e77a7c11f6d984371
Gergely Németh <ngerle@gmail.com>
2017-06-13 22:14:19 +0200

    Add ability to use the keyboard to enter numbers

--- calculator.js
+++ calculator.js
@@ -1,18 +1,32 @@
 var display = document.getElementById('display');
 var els = document.querySelectorAll('a.number');
+var fullNumber = display.innerHTML;
+
+display.innerHTML = "0";
 
 function displayNumbers(number) {
-    var fullNumber = display.innerHTML;
-
     if (fullNumber.split('.').length > 1 && number === '.') {
         number = '';
     }
-
     if (fullNumber.length < 17) {
         fullNumber += number;  
     }
-
     display.innerHTML = fullNumber;
+}
+
+function keyPress(event) {
+    if (event.key === 'c') {
+        display.innerHTML = '0';
+        fullNumber = '';
+    }
+    if (event.key === ',') {
+        displayNumbers('.');
+    }
+    for (let i=0, n=els.length; i<n; ++i) {
+        if (event.key === els[i].innerHTML) {
+            displayNumbers(els[i].innerHTML);
+        }
+    }  
 }
 
 for (let i=0, n=els.length; i<n; ++i) {
@@ -21,3 +35,4 @@ for (let i=0, n=els.length; i<n; ++i) {
     }
 }
 
+document.addEventListener("keypress", keyPress);
